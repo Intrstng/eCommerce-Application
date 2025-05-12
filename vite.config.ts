2e1911020123c0aa9beff36654fa898b470c7b6a
Intrstng <intrstg@gmail.com>
2025-05-12 17:09:04 +0300

    refactor: disable modules chuncking

--- vite.config.ts
+++ vite.config.ts
@@ -16,75 +16,75 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths(), addRootDivPlugin()],
 
   build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules/react') ||
-              id.includes('node_modules/react-dom') ||
-              id.includes('node_modules/scheduler') ||
-              id.includes('node_modules/react-router-dom') ||
-              id.includes('node_modules/react-redux')) {
-            return 'vendor-react';
-          }
-
-          if (id.includes('node_modules/@mui') ||
-              id.includes('node_modules/@emotion')) {
-            return 'vendor-mui';
-          }
-
-          if (id.includes('node_modules')) {
-            const match = id.match(/node_modules\/(.*?)\//);
-            if (match) {
-              const packageName = match[1];
-              if ([
-                'axios',
-                'yup',
-                'react-hook-form',
-                '@hookform/resolvers',
-                'react-toastify',
-                'uuid',
-                '@lottiefiles/dotlottie-react'
-              ].includes(packageName)) {
-                return `vendor-${packageName.replace(/[^a-zA-Z0-9]/g, '-')}`;
-              }
-              return 'vendor-libs';
-            }
-          }
-
-          if (id.includes('src/features/')) {
-            const match = id.match(/src\/features\/(.*?)\//);
-            if (match) {
-              return `feature-${match[1]}`;
-            }
-          }
-
-          if (id.includes('src/common')) {
-            return 'common';
-          }
-
-          if (id.includes('src/assets')) {
-            return 'assets';
-          }
-
-          if (id.includes('src/app')) {
-            return 'app-core';
-          }
-
-          if (id.includes('src/app/store')) {
-            return 'redux-core';
-          }
-
-          if (id.includes('src/common/routes') || id.includes('src/common/pages')) {
-            const match = id.match(/src\/common\/(routes|pages)\/([^\/]+)/);
-            if (match) return `route-${match[2]}`;
-          }
-        },
-        chunkFileNames: 'js/[name]-[hash].js',
-        entryFileNames: 'js/[name]-[hash].js',
-        assetFileNames: '[ext]/[name]-[hash].[ext]',
-      },
-    },
+    chunkSizeWarningLimit: 3000,
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id) {
+    //       if (id.includes('node_modules/react') ||
+    //           id.includes('node_modules/react-dom') ||
+    //           id.includes('node_modules/scheduler') ||
+    //           id.includes('node_modules/react-router-dom') ||
+    //           id.includes('node_modules/react-redux')) {
+    //         return 'vendor-react';
+    //       }
+    //
+    //       if (id.includes('node_modules/@mui') ||
+    //           id.includes('node_modules/@emotion')) {
+    //         return 'vendor-mui';
+    //       }
+    //
+    //       if (id.includes('node_modules')) {
+    //         const match = id.match(/node_modules\/(.*?)\//);
+    //         if (match) {
+    //           const packageName = match[1];
+    //           if ([
+    //             'axios',
+    //             'yup',
+    //             'react-hook-form',
+    //             '@hookform/resolvers',
+    //             'react-toastify',
+    //             'uuid',
+    //             '@lottiefiles/dotlottie-react'
+    //           ].includes(packageName)) {
+    //             return `vendor-${packageName.replace(/[^a-zA-Z0-9]/g, '-')}`;
+    //           }
+    //           return 'vendor-libs';
+    //         }
+    //       }
+    //
+    //       if (id.includes('src/features/')) {
+    //         const match = id.match(/src\/features\/(.*?)\//);
+    //         if (match) {
+    //           return `feature-${match[1]}`;
+    //         }
+    //       }
+    //
+    //       if (id.includes('src/common')) {
+    //         return 'common';
+    //       }
+    //
+    //       if (id.includes('src/assets')) {
+    //         return 'assets';
+    //       }
+    //
+    //       if (id.includes('src/app')) {
+    //         return 'app-core';
+    //       }
+    //
+    //       if (id.includes('src/app/store')) {
+    //         return 'redux-core';
+    //       }
+    //
+    //       if (id.includes('src/common/routes') || id.includes('src/common/pages')) {
+    //         const match = id.match(/src\/common\/(routes|pages)\/([^\/]+)/);
+    //         if (match) return `route-${match[2]}`;
+    //       }
+    //     },
+    //     chunkFileNames: 'js/[name]-[hash].js',
+    //     entryFileNames: 'js/[name]-[hash].js',
+    //     assetFileNames: '[ext]/[name]-[hash].[ext]',
+    //   },
+    // },
   },
 
   server: {
