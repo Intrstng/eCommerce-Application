05ddc01374c73fa4302356e1b0fa3bad1ddbe719
JaKsHaZbi <joker_hazbi@mail.ru>
2025-05-14 19:44:28 +0300

    fix: build proj

--- src/common/utils/i18n.ts
+++ src/common/utils/i18n.ts
@@ -1,29 +1,24 @@
+import type { InitOptions } from 'i18next';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-async function initializeI18n() {
-  await i18n
-    .use(HttpBackend)
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-      fallbackLng: 'en',
-      debug: true,
-      interpolation: {
+const i18nOptions: InitOptions = {
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: {
         escapeValue: false,
-      },
-      backend: {
+    },
+    backend: {
         loadPath: '/locales/{{lng}}/{{ns}}.json',
-      },
-      detection: {
+    },
+    detection: {
         order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
         caches: ['cookie'],
-      },
-    });
-}
+    },
+};
 
-initializeI18n().catch(console.error);
+export const i18nInitPromise = i18n.use(HttpBackend).use(LanguageDetector).use(initReactI18next).init(i18nOptions);
 
-export default i18n;
+export { default } from 'i18next';
