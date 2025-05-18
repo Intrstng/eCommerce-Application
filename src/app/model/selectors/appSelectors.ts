3010d280230a1265f19200c70542ccffb4551782
Liubou Levitskaya <lyubasaraeva@gmail.com>
2025-05-18 03:42:51 +0300

    feat: add global loading indicator, error handling and app state selectors to App

---
+++ src/app/model/selectors/appSelectors.ts
@@ -0,0 +1,6 @@
+import type { AppRootState } from 'app/store';
+import type { AppError, Status } from 'app/model/types';
+
+export const statusSelector = (state: AppRootState): Status => state.app.status;
+export const errorSelector = (state: AppRootState): AppError => state.app.error;
+export const isInitializedSelector = (state: AppRootState): boolean => state.app.isInitialized;
