b6c06fcfd924230148139ba7fd11e29989ce83cf
Liubou Levitskaya <lyubasaraeva@gmail.com>
2025-06-16 01:39:54 +0300

    feat: develop comprehensive "About Us" page with team details and RS School link integration

---
+++ src/common/pages/AboutPage/styles.aboutPage.ts
@@ -0,0 +1,37 @@
+import { genStyles } from '../../utils/generate-styles.ts';
+
+export const STYLES = genStyles({
+    aboutPageContent: {
+        width: 'calc(100% - 2rem)',
+        maxWidth: '1180px',
+        margin: '0 auto',
+        padding: '0 1rem',
+    },
+    aboutContent: {
+        width: '100%',
+    },
+    aboutTitle: {
+        marginBottom: '0.4rem',
+        fontFamily: '"Cormorant", serif',
+        fontWeight: '500',
+        fontSize: '3.25rem',
+        lineHeight: '3.9rem',
+    },
+    teamMembersContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '3rem',
+    },
+    rsSchoolLogoContainer: {
+        fontFamily: '"Jost", sans-serif',
+        fontSize: '1.1rem',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '0.5rem',
+        marginTop: '1.5rem',
+        img: {
+            width: '4rem',
+        },
+    },
+});
