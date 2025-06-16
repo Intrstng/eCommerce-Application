5a7ee3865886a5baa41e0623adf546445807b418
Liubou Levitskaya <lyubasaraeva@gmail.com>
2025-06-16 04:50:08 +0300

    fix: make burger menu slide animation smoother

--- src/common/components/Header/BurgerMenu/BurgerMenu.tsx
+++ src/common/components/Header/BurgerMenu/BurgerMenu.tsx
@@ -5,7 +5,7 @@ import icons from '../../../../assets/icons/icons.tsx';
 import { Link, NavLink } from 'react-router-dom';
 import { CATEGORIES, PATH } from '../../../enums';
 import { useAppDispatch, useAppSelector } from '../../../hooks';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { CartLogo } from '../../CartLogo/CartLogo';
 import type { Cart, LineItem } from '@commercetools/platform-sdk';
 import { cartSelector } from '../../../../features/cart/model/selectors/cartSelectors';
@@ -36,68 +36,72 @@ export const BurgerMenu: React.FC<BurgerMenuProps> = ({ isOpen, onClose }) => {
         onClose();
     };
 
-    if (!isOpen) {
-        return null;
-    }
-
     return (
-        <div className={S.burgerMenuOverlay} onClick={onClose}>
-            <motion.div
-                className={S.burgerMenuContent}
-                onClick={event => {
-                    event.stopPropagation();
-                }}
-                initial={{ x: '-100%' }}
-                animate={{ x: 0 }}
-                exit={{ x: '-100%' }}
-                transition={{ type: 'spring', stiffness: 200, damping: 25 }}
-            >
-                <div className={S.topBar}>
-                    <div className={S.closeIcon} onClick={onClose}>
-                        <icons.close />
-                    </div>
-                    <Link to={PATH.PAGE_ROOT} onClick={onClose}>
-                        <icons.logo className={S.logo} />
-                    </Link>
-                    <CartLogo
-                        counter={cartQuantity}
-                        size="1.5rem"
-                        counterClassName={S.cartBurgerCounter}
-                        onClickCB={onClose}
-                    />
-                </div>
-                <div className={S.navLinks}>
-                    <NavLink to={`${PATH.CATALOG}?productType=Earrings`} className={S.navLink} onClick={onClose}>
-                        {CATEGORIES.EARRINGS}
-                    </NavLink>
-                    <NavLink to={`${PATH.CATALOG}?productType=Rings`} className={S.navLink} onClick={onClose}>
-                        {CATEGORIES.RINGS}
-                    </NavLink>
-                    <NavLink to={`${PATH.CATALOG}?productType=Brooches`} className={S.navLink} onClick={onClose}>
-                        {CATEGORIES.BROOCHES}
-                    </NavLink>
-                    <NavLink to={PATH.CATALOG} className={S.navLink} onClick={onClose}>
-                        ALL PRODUCTS
-                    </NavLink>
-                    <NavLink to={PATH.ABOUT} className={S.navLink} onClick={onClose}>
-                        ABOUT US
-                    </NavLink>
-                </div>
-                {isLoggedIn ? (
-                    <>
-                        <NavLink to={PATH.PROFILE_MAIN} className={S.button} onClick={onClose}>
-                            ACCOUNT
-                        </NavLink>
-                        <NavLink to={PATH.SIGNIN} onClick={handleLogout} aria-label="logout" className={S.button}>
-                            Logout
-                        </NavLink>
-                    </>
-                ) : (
-                    <NavLink to={PATH.SIGNIN} className={S.button} onClick={onClose}>
-                        LOG IN
-                    </NavLink>
-                )}
-            </motion.div>
-        </div>
+        <AnimatePresence>
+            {isOpen && (
+                <motion.div
+                    className={S.burgerMenuOverlay}
+                    onClick={onClose}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                >
+                    <motion.div
+                        className={S.burgerMenuContent}
+                        onClick={e => e.stopPropagation()}
+                        initial={{ x: '-100%' }}
+                        animate={{ x: 0 }}
+                        exit={{ x: '-100%' }}
+                        transition={{ type: 'tween', ease: 'easeInOut', duration: 0.5 }}
+                    >
+                        <div className={S.topBar}>
+                            <div className={S.closeIcon} onClick={onClose}>
+                                <icons.close />
+                            </div>
+                            <Link to={PATH.PAGE_ROOT} onClick={onClose}>
+                                <icons.logo className={S.logo} />
+                            </Link>
+                            <CartLogo
+                                counter={cartQuantity}
+                                size="1.5rem"
+                                counterClassName={S.cartBurgerCounter}
+                                onClickCB={onClose}
+                            />
+                        </div>
+                        <div className={S.navLinks}>
+                            <NavLink to={`${PATH.CATALOG}?productType=Earrings`} className={S.navLink} onClick={onClose}>
+                                {CATEGORIES.EARRINGS}
+                            </NavLink>
+                            <NavLink to={`${PATH.CATALOG}?productType=Rings`} className={S.navLink} onClick={onClose}>
+                                {CATEGORIES.RINGS}
+                            </NavLink>
+                            <NavLink to={`${PATH.CATALOG}?productType=Brooches`} className={S.navLink} onClick={onClose}>
+                                {CATEGORIES.BROOCHES}
+                            </NavLink>
+                            <NavLink to={PATH.CATALOG} className={S.navLink} onClick={onClose}>
+                                ALL PRODUCTS
+                            </NavLink>
+                            <NavLink to={PATH.ABOUT} className={S.navLink} onClick={onClose}>
+                                ABOUT US
+                            </NavLink>
+                        </div>
+                        {isLoggedIn ? (
+                            <div className={S.authButtons}>
+                                <NavLink to={PATH.PROFILE_MAIN} className={S.button} onClick={onClose}>
+                                    ACCOUNT
+                                </NavLink>
+                                <NavLink to={PATH.SIGNIN} onClick={handleLogout} className={S.button}>
+                                    LOGOUT
+                                </NavLink>
+                            </div>
+                        ) : (
+                            <NavLink to={PATH.SIGNIN} className={S.button} onClick={onClose}>
+                                LOG IN
+                            </NavLink>
+                        )}
+                    </motion.div>
+                </motion.div>
+            )}
+        </AnimatePresence>
     );
 };
