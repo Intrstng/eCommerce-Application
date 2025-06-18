60b6b7c661b8bc0bab44d80e315728e022f81d5f
Intrstng <intrstg@gmail.com>
2025-06-18 17:14:13 +0300

    feat: add text slice of product slider cart title

--- src/common/components/ProductSliderCard/ProductSliderCard.tsx
+++ src/common/components/ProductSliderCard/ProductSliderCard.tsx
@@ -6,6 +6,9 @@ import Box from '@mui/material/Box';
 import { formatPrice } from '../../../features/catalog/utils/format-price';
 import { useNavigate } from 'react-router-dom';
 import { PATH } from '../../enums';
+import { customSliceString } from '../../utils/custom-slice-string';
+
+const SLIDER_CARD_TITLE_LENGTH: number = 35;
 
 export const ProductSliderCard: FC<ProductSliderCardProps> = ({ productItemData }) => {
   const navigate = useNavigate();
@@ -14,13 +17,15 @@ export const ProductSliderCard: FC<ProductSliderCardProps> = ({ productItemData 
       navigate(`${PATH.PRODUCT_MAIN}/${productItemData?.id ?? ''}`);
     };
 
+    const cardTitle: string = customSliceString(productItemData.name.en, SLIDER_CARD_TITLE_LENGTH);
+
     return (
         <Box className={S.productCard} onClick={cardClickHandler}>
             <Box className={S.imageContainer}>
                 <img className={S.image} src={productItemData.images[0] ?? noImage} alt={'Product item slide'} />
             </Box>
             <Box className={S.infoContainer}>
-                <Box className={S.name}>{productItemData.name.en}</Box>
+                <Box className={S.name}>{cardTitle}</Box>
                 <Box className={S.price}>{currentFormattedPrice}</Box>
             </Box>
         </Box>
--- src/common/pages/CartPage/CartItem.tsx
+++ src/common/pages/CartPage/CartItem.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react';
+import { useMemo } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -14,19 +15,15 @@ import plusIcon from '../../../assets/icons/plus.svg';
 import minusIcon from '../../../assets/icons/minus.svg';
 import { PRICE_STYLES } from '../../styles/price.styles';
 import type { Status } from 'app/model/types';
-import { cartStatusSelector } from '../../../features/cart/model/selectors/cartSelectors';
+import { cartSelector, cartStatusSelector } from '../../../features/cart/model/selectors/cartSelectors';
 import { CartItemSkeleton } from './CartItemSkeleton';
 import type { CartItemProps } from './interfaces';
-import type { LineItemWithDiscountedPrice } from './interfaces';
-import { useMemo } from 'react';
-import type { CatalogProduct } from '../../../features/catalog/api/catalogApi.interfaces';
 import {
-    promoCodeSelector,
     availablePromoCodesSelector,
+    promoCodeSelector,
 } from '../../../features/discount/model/selectors/discountSelectors';
 import type { DiscountCode } from '@commercetools/platform-sdk';
 import { checkIsPromoCodeApplied } from '../../utils/check-is-promocode-applied';
-import { cartSelector } from '../../../features/cart/model/selectors/cartSelectors';
 
 export const CartItem: FC<CartItemProps> = ({ item, availableQuantity, catalogProduct }) => {
     const isCartLoading: string = useAppSelector<Status>(cartStatusSelector);
